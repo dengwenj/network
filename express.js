f5ac59e7e32dda2430f0154409bdd114ce3179a2
朴睦 <927561237@qq.com>
2024-03-01 22:28:05 +0800

    sse使用

--- express.js
+++ express.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const fs = require('fs')
 
 const app = express()
 
@@ -21,6 +22,29 @@ app.post('/api/posttest', (req, res) => {
   res.send({
     code: 200
   })
+})
+
+// SSE
+app.get('/api/sse', (req, res) => {
+   res.setHeader('Access-Control-Allow-Origin', '*')
+  res.writeHead(200, {
+    // 开启 sse，核心代码设置对应的请求头
+    'Content-Type': 'text/event-stream'
+  })
+
+  const txt = fs.readFileSync('./demo.txt', 'utf8')
+  console.log(txt, 'txttxttxt');
+  const arr = txt.split('')
+  let current = 0
+
+  let timer = setInterval(() => {
+    if (current < arr.length) {
+      res.write(`data: ${arr[current]}`)
+      current++
+    } else {
+      clearInterval(timer)
+    }
+  }, 300)
 })
 
 app.listen(3000, () => {
